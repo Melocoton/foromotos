ade846ba2e257d9d8a641ff119e83ef8124e8f31
Oscar Muñoz Moreno <oscar_novesano_mm@hotmail.com>
2018-07-13 00:33:33 +0200

    HTML actualizado

---
+++ src/settings.php
@@ -0,0 +1,27 @@
+<?php
+return [
+    'settings' => [
+        'displayErrorDetails' => true, // set to false in production
+        'addContentLengthHeader' => false, // Allow the web server to send the content-length header
+
+        //connection settings
+        'db' => [
+            'host' => '192.168.1.9',
+            'user' => 'foromotos',
+            'pass' => 'foromotos',
+            'dbname' => 'foromotos'
+        ],
+
+        // Renderer settings
+        'renderer' => [
+            'template_path' => __DIR__ . '/../templates/',
+        ],
+
+        // Monolog settings
+        'logger' => [
+            'name' => 'slim-app',
+            'path' => isset($_ENV['docker']) ? 'php://stdout' : __DIR__ . '/../logs/app.log',
+            'level' => \Monolog\Logger::DEBUG,
+        ],
+    ],
+];
